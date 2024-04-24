fcb5c6800264a2f8624dc2869ab8d3d8360cecc8
Jose Luis Cruz <j0z3ph@gmail.com>
2024-04-24 11:37:50 -0600

    Ejemplos

---
+++ C++/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief
+ * @version 0.1
+ * @date 2024-03-22
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <iostream>
+#include <vector>
+#include "tools.hpp"
+
+using namespace std;
+
+int main()
+{
+    vector<string> opciones;
+    opciones.push_back("Opcion 1");
+    opciones.push_back("Opcion 2");
+    opciones.push_back("Salir");
+
+    int opcion = showMenu(opciones, "Menu");
+
+    while(opcion != 2) {
+        pausa();
+        opcion = showMenu(opciones, "Menu");
+    }
+
+    return 0;
+}
